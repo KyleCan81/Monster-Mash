687bdb60c2c586aedd735557260dd3ed40b8c8c3
KyleCan81 <Kylecan8112@gmail.com>
2024-10-23 10:29:45 -0400

    Monster Mash

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function Monster1(name, toes, eyes, arms) {
+    this.name = name;
+    this.toes = toes;
+    this.eyes = eyes;
+    this.arms = arms;
+}
+
+var quayMonster1 = new Monster1('Agatha', 3, 2, 4)
+
+console.log(quayMonster1)
